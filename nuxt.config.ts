0677da6338b13b22bc368ed789a0f78ac4b68363
ouahib-abderrazzak <ouahib.abderrazzak@hi5group.co.uk>
2024-01-26 23:41:10 +0100

    Applying Plus Jakarta Sans font on every single html element, configuring SCSS, adding _vars.scss and global.scss.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 export default defineNuxtConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/css/_vars.scss" as *;',
+        },
+      },
+    },
+  },
+  css: ['~/assets/css/global.scss'],
   modules: ['@nuxtjs/google-fonts'],
   googleFonts: {
     families: {
