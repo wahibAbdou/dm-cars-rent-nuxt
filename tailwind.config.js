475ed9e70e9d0916ee25fd213e39ada1c9f412d4
ouahib-abderrazzak <ouahib.abderrazzak@hi5group.co.uk>
2024-01-27 00:51:06 +0100

    Extracting UI colors as variables and inject them into TailwindCSS configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      // Injecting UI vars in TailwindCSS
+      colors: {
+        'primary': 'var(--primary-color)',
+        'secondary-color': 'var(--secondary-color)',
+        'bg-color': 'var(--bg-color)',
+        'bg-label': 'var(--bg-label)',
+        'bg-heart-icon-active': 'var(--bg-heart-icon-active)',
+        'bg-heart-icon-default': 'var(--bg-heart-icon)',
+        'bg-heart-icon-border': 'var(--bg-heart-icon-border)',
+      },
+      fontFamily: {
+        'plus-jakarta-sans': ['var(--plus-jakarta-sans-font)'],
+      },
+    },
   },
   plugins: [],
 }
