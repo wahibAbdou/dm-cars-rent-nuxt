27eafc2a5f5264983b26c78c5334ed74eddafb93
ouahib-abderrazzak <ouahib.abderrazzak@hi5group.co.uk>
2024-01-27 13:29:43 +0100

    Add Button utility component

---
+++ components/utils/Button.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+interface ButtonProps {
+	text?: string;
+	classes?: 'btn-primary' | 'btn-light';
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+	text: 'Default Primary Button',
+	classes: 'btn-primary',
+});
+</script>
+
+<template>
+	<button :class="classes">{{ text }}</button>
+</template>
+
+<style lang="scss" scoped>
+button {
+	@apply text-white px-4 py-3 rounded;
+}
+
+.btn-primary {
+	@apply bg-primary;
+	@apply focus:ring-4 focus:ring-primary-light;
+}
+
+.btn-light {
+	@apply bg-primary-light;
+	@apply focus:ring-4 focus:ring-blue-200;
+}
+</style>
